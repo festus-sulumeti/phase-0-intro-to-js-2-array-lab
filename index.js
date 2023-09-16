6eeffb45e17760f509ef18083161a26c83d8ddbc
festus-sulumeti <sulumetifestus@outlook.com>
2023-09-16 09:46:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Write your solution here!
+
+let cats;
+cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) 
+{
+    cats.push(name);
+    console.log(cats);
+}
+
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name) 
+{
+    cats.unshift(name);
+    console.log(cats);
+}
+
+function destructivelyRemoveLastCat() 
+{
+    cats.pop();
+    console.log(cats);
+}
+
+destructivelyRemoveLastCat();
+
+function destructivelyRemoveFirstCat() 
+{
+    cats.shift();
+    console.log(cats);
+}
+
+function appendCat(name) 
+{
+    let newCats;
+    newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) 
+{
+    let newCats;
+    newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeFirstCat()    
+{
+    let newCats;
+    newCats = cats.slice(1);
+    return newCats;
+}
+
+function removeLastCat() 
+{
+    let newCats;
+    newCats = cats.slice(0, cats.length - 1);
+   return newCats;
+}
